e6ef3817d35c1b093b7f23c25fe28e011812eed2
corderom <joseph.cordero@gmail.com>
2020-03-12 19:11:17 -0500

    Minor format changes

--- Utils/src/RunLumiRangeMap.cc
+++ Utils/src/RunLumiRangeMap.cc
@@ -1,28 +1,31 @@
 #include "BaconAna/Utils/interface/RunLumiRangeMap.h"
 #include "BaconAna/Utils/interface/Jzon.h"
 
-//--------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 bool RunLumiRangeMap::HasRunLumi(const RunLumiPairType &runLumi) const
 {
-  // Check if a given run,lumi pair is included in the mapped lumi ranges
+	//Check if a given run,lumi pair is included in the mapped lumi ranges
 
-  //check if run is included in the map
-  MapType::const_iterator it = fMap.find(runLumi.first);
-  if (it!=fMap.end()) {
-    //check lumis
-    const MapType::mapped_type &lumiPairList = it->second;
-    for (MapType::mapped_type::const_iterator jt = lumiPairList.begin(); jt<lumiPairList.end(); ++jt) {
-      if (runLumi.second >= jt->first && runLumi.second <= jt->second) {
-        //found lumi in accepted range
-        return kTRUE;
-      }
-    }
-  }
+	//check if run is included in the map
+	MapType::const_iterator it = fMap.find(runLumi.first);
+	if (it!=fMap.end()) {
+		//check lumis
+		const MapType::mapped_type &lumiPairList = it->second;
+		for (MapType::mapped_type::const_iterator jt = lumiPairList.begin(); jt<lumiPairList.end(); ++jt) {
+			if (runLumi.second >= jt->first && runLumi.second <= jt->second) {
+				//found lumi in accepted range
+				return kTRUE;
+			}
+		}
+	}
 
-  return kFALSE;
+	//for (MapType::mapped_type::const_iterator jt = lumiPairList.begin(); jt<lumiPairList.end(); ++jt) {
+	//	if (runLumi.second >= jt->first && runLumi.second <= jt->second) 
+	//		return kTRUE;	
+	//}
+	return kFALSE;
 
 }
-
 //--------------------------------------------------------------------------------------------------
 void RunLumiRangeMap::AddJSONFile(const std::string &filepath) 
 {
@@ -30,20 +33,20 @@ void RunLumiRangeMap::AddJSONFile(const std::string &filepath)
   //read json without boost bullshit
   Jzon::Object rootNode;
   Jzon::FileReader::ReadFile(filepath, rootNode);
-    
+
   //Build a map of lumi stuff 
-  for (Jzon::Object::iterator it = rootNode.begin(); it != rootNode.end(); ++it) { 
-    unsigned int runNumber = (unsigned int) atoi(((*it).first).c_str());
-    MapType::mapped_type &lumiPairList = fMap[runNumber];
-    const Jzon::Array &pLumis = rootNode.Get((*it).first).AsArray();
-    for (Jzon::Array::const_iterator it = pLumis.begin(); it != pLumis.end(); ++it) { 
-      Jzon::Array lumiPairTree = (*it).AsArray();
-      if (lumiPairTree.GetCount()==2) {
-        UInt_t firstLumi = (*   (lumiPairTree.begin()) ).ToInt();
-        UInt_t lastLumi =  (*(++(lumiPairTree.begin()))).ToInt();
-        lumiPairList.push_back(std::pair<UInt_t,UInt_t>(firstLumi,lastLumi));
-      }
-    }
+  for (Jzon::Object::iterator it = rootNode.begin(); it != rootNode.end(); ++it) {
+	unsigned int runNumber = (unsigned int) atoi(((*it).first).c_str());
+	MapType::mapped_type &lumiPairList = fMap[runNumber];
+	const Jzon::Array &pLumis = rootNode.Get((*it).first).AsArray();
+	for (Jzon::Array::const_iterator it = pLumis.begin(); it != pLumis.end(); ++it) { 
+		Jzon::Array lumiPairTree = (*it).AsArray();
+		if (lumiPairTree.GetCount()==2) {
+			UInt_t firstLumi = (*   (lumiPairTree.begin()) ).ToInt();
+			UInt_t lastLumi =  (*(++(lumiPairTree.begin()))).ToInt();
+			lumiPairList.push_back(std::pair<UInt_t,UInt_t>(firstLumi,lastLumi));
+		}
+	}
   }
 
   //dump run and lumi ranges from MapType structure to verify correct json parsing
